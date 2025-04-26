f2754007f8fbe7d1ca6ee6bc7e14b2e6ad3d87ce
Cordeva <ahmed.abdelaaall@gmail.com>
2025-04-26 16:04:17 +0300

    added the stored theme directly on layout to be rendered immediately without flashing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* setting the theme on server to be shown on render without the flashing that happens if its set via useEffect */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  var storedTheme = localStorage.getItem('theme');
+                  var theme = storedTheme === 'dark' ? 'dark' : 'light';
+                  document.documentElement.classList.add(theme);
+                } catch (e) {
+                  document.documentElement.classList.add('light');
+                }
+              })();
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
